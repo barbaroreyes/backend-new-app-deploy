c7516a59d67a22f338f855292b3a8c23947d814e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-18 15:49:47 -0500

    update

--- models/7-wondefull.js
+++ models/7-wondefull.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema
 const sevenWSchema = new Schema({
     name: String,
     img: String,
-    description: String,
+    description:String
 
 },{timestamps:true}
 )
 
                            //collections- by default will make a lowercase
-const SevenW = mongoose.model( 'SevenW ', sevenWSchema )
+const SevenW = mongoose.model('SevenW',sevenWSchema )
 //export the schema 
 module.exports= SevenW
